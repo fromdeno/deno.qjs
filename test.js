68757722975b201e92b902b49c84253419a63818
Thomas Rory Gummerson <thomas@gummerson.no>
2021-11-24 15:09:09 +0100

    Slight build fix

--- test.js
+++ test.js
@@ -4,15 +4,6 @@ import {
 } from "https://deno.land/std@0.115.1/streams/conversion.ts";
 
 (async () => {
-  // code to conditionally import deno.qjs
-  // #region
-  /** @type {typeof globalThis.Deno} */
-  const Deno = "Deno" in globalThis
-    ? globalThis.Deno
-    : await import("./Deno.js");
-  "Deno" in globalThis || (globalThis.Deno = Deno);
-  // #endregion
-
   const file = await Deno.open("test.txt", { write: true });
   for await (const buf of iterateReader(Deno.stdin, { bufSize: 10 })) {
     await writeAll(file, buf);
